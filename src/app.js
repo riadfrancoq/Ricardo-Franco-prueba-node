ba7cdf997a457cb340d07307ba5a1bac29e92446
riadfrancoq <campusricardo2023@gmail.com>
2024-02-29 17:29:49 -0500

    feat: :see_no_evil: add .env to gitignore

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import express, {json} from "express";
-import { check } from "express-validator";
-
+// cambie esto con cuidado 
 import productosRouter from './routes/productos.routes.js';
 const app = express();
 
-app.use(express.json());
+app.use(json());
 app.use('/api/', productosRouter);
 export default app;
